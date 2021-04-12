a530e691a5e8f0b99d45df43c5436e7f63abec0c
Marco de boer <76598975+Mdb1990@users.noreply.github.com>
2021-04-12 22:25:04 +0200

    Add files via upload

---
+++ Opdracht21JS/script.js
@@ -0,0 +1,23 @@
+const age = 16;
+
+if (age >= 18) {
+    console.log("You are allowed inside");
+} else {
+    console.log("You are not allowed inside")
+}
+
+const isFemale = true;
+
+if (isFemale == true) {
+    console.log("you are allowed to enter on ladies night")
+} else {
+    console.log("You are a man and not allowed to enter on ladies night")
+}
+
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log("You are allowed to drive!!")
+} else {
+    console.log("You are to drunk to drive!!")
+}
